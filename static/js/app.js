9fd10b790c98b9a63442a238aa93f044d11ed322
Zach-EE <zachholloway.n@gmail.com>
2022-08-19 03:23:40 +0000

    storm commit

--- static/js/app.js
+++ static/js/app.js
@@ -16,52 +16,81 @@ Vue.component('pokemon-card', {
 
             </div>
             <div class="card-footer btn-group d-flex justify-content-around" style="width: 21rem;">
-                <a href="#" class="rounded btn btn-outline-success btn-sm btn-block float-start mr-1 w-100">
+                <a class="rounded btn btn-outline-success btn-sm btn-block float-start mr-1 w-100" @click.prevent="catchPokemon(pokemon)">
                     catch<br>[[pokemon.name]]
                 </a>
-                <a href="#" class="rounded btn btn-outline-warning btn-sm float-none mx-1 w-100">edit<br>[[pokemon.name]]</a>
-                <a href="#" class="rounded btn btn-outline-danger btn-sm btn-block ml-1 w-100">release<br>[[pokemon.name]]</a>
+                <a class="rounded btn btn-outline-warning btn-sm float-none mx-1 w-100" @click.prevent="releasePokemon(pokemon)">edit<br>[[pokemon.name]]</a>
+                <a class="rounded btn btn-outline-danger btn-sm btn-block ml-1 w-100" @click.prevent="releasePokemon(pokemon)">release<br>[[pokemon.name]]</a>
             </div>
         </div>
     `,
+  methods: {
+    catchPokemon: function (pokemon) {
+      this.$emit('catch', pokemon);
+    },
+    releasePokemon: function (pokemon) {
+      this.$emit('release', pokemon);
+    },
+  },
 });
 
-const vm = new Vue ({
-    el: '#app',
-    delimiters:['[[',']]'],
-    data: {
-        pokemon_list: [],
-        csrfToken: "",
-        users: [],
-        type: []
+const vm = new Vue({
+  el: '#app',
+  delimiters: ['[[', ']]'],
+  data: {
+    pokemon_list: [],
+    csrfToken: '',
+    users: [],
+    type: [],
+  },
+  methods: {
+    loadPokemon: function () {
+      console.log('loading pokemon');
+      axios({
+        method: 'get',
+        url: '/apis/v1/pokemon/',
+      }).then((response) => {
+        console.log(response.data);
+        this.pokemon_list = response.data;
+      });
     },
-    methods: {
-        loadPokemon: function(){
-            console.log('loading pokemon')
-            axios({
-                method: 'get',
-                url: '/apis/v1/pokemon/',
-            }).then((response) => {
-                console.log(response.data)
-                this.pokemon_list = response.data
-            })
-        },
-        loadUsers: function(){
-            console.log('loading users');
-            axios({
-              method: 'get',
-              url: '/apis/v1/users/',
-            }).then((response) => {
-              console.log(response.data);
-              this.users = response.data;
-            });            
+    loadUsers: function () {
+      console.log('loading users');
+      axios({
+        method: 'get',
+        url: '/apis/v1/users/',
+      }).then((response) => {
+        console.log(response.data);
+        this.users = response.data;
+      });
+    },
+    catchPokemon: function (pokemon) {
+      console.log('catch ', pokemon.name);
+      let pokeID = 
+      axios({
+        method: 'patch',
+        url: `/apis/v1/pokemon/${this.pokemon.id}/`,
+        headers: {
+            'X-CSRFToken': this.csrfToken
         }
+      }).then((response) => {
+        this.pokemon_list = response.data
+      }).catch((err)=>{
+        console.log(err)
+      })
     },
-    created: function () {
-        this.loadPokemon()
-        this.loadUsers()
+    releasePokemon: function (pokemon) {
+      console.log('release ', pokemon.name);
+      this.$emit('release', pokemon);
     },
-    mounted() {
-        this.csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value
-    },
-})
+  },
+  created: function () {
+    this.loadPokemon()
+    this.loadUsers()
+  },
+  mounted() {
+    this.csrfToken = document.querySelector(
+      'input[name=csrfmiddlewaretoken]'
+    ).value;
+  },
+});
